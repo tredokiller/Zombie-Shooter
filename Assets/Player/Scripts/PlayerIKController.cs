9a07338ab14e74915513e9f119988c42c2616502
foxstudent103020 <vlad.tredo86@gmail.com>
2023-07-07 19:28:07 +0300

    Added Footstep sounds, bugs fixed

--- Assets/Player/Scripts/PlayerIKController.cs
+++ Assets/Player/Scripts/PlayerIKController.cs
@@ -1,8 +1,18 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Timers;
+using Common.CommonScripts;
+using Common.CommonScripts.States;
+using Managers;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
+using UnityEngine.Serialization;
 using Weapons.Scripts;
+using Zenject;
+using Random = UnityEngine.Random;
+using Timer = Common.CommonScripts.Timer;
 
 namespace Player.Scripts
 {
@@ -12,7 +22,26 @@ namespace Player.Scripts
         [SerializeField] private Transform leftHandPosition;
         [SerializeField] private Transform rightHandPosition;
 
+        [SerializeField] private Transform[] foots;
+
+        [SerializeField] private AudioClip[] grassFootStepSounds;
+        [SerializeField] private AudioClip[] roadFootStepSounds;
+
+        [SerializeField] private LayeredAudioClips[] footStepsDictionary;
+
+        private const float DistanceToGroundFromFoot = 0.35f;
+        private const float MinDurationBetweenStepSounds = 0.3f;
+        
         private PlayerController _playerController;
+        private ISfxManager _sfxManager;
+
+        private bool _canProduceFootstepSound = true;
+
+        [Inject]
+        private void Constructor(ISfxManager sfxManager)
+        {
+            _sfxManager = sfxManager;
+        }
 
         private void Awake()
         {
@@ -27,6 +56,20 @@ namespace Player.Scripts
         private void Update()
         {
             UpdateHandsPosition();
+            CheckFootSteps();
+        }
+        
+        private void PlayFootstepSound(RaycastHit groundHit)
+        {
+            var currentLayer = groundHit.collider.gameObject.layer;
+            foreach (var layeredAudioClip in footStepsDictionary)
+            {
+                if ((layeredAudioClip.LayerMask & (1 << currentLayer)) != 0)
+                {
+                    int randomIndex = Random.Range(0, layeredAudioClip.AudioClips.Length);
+                    _sfxManager.MakeSound(layeredAudioClip.AudioClips[randomIndex] , 0.15f);
+                }
+            }
         }
 
         private void UpdateHandsPosition()
@@ -41,6 +84,23 @@ namespace Player.Scripts
 
                 rightHandPosition.position = rightHand.position;
                 rightHandPosition.rotation = rightHand.rotation;
+            }
+        }
+
+        private void CheckFootSteps()
+        {
+            RaycastHit hit;
+            if (_playerController.GetState() != State.Idle && _canProduceFootstepSound)
+            {
+                foreach (var foot in foots)
+                {
+                    if (Physics.Raycast(foot.transform.position, Vector3.down, out hit, DistanceToGroundFromFoot))
+                    {
+                        PlayFootstepSound(hit);
+                        _canProduceFootstepSound = false;
+                        Timer.StartTimer(MinDurationBetweenStepSounds, () => _canProduceFootstepSound = true);
+                    } 
+                }
             }
         }
         
